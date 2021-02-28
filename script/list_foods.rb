cfa79759ab3f2db2fc390c2480d975310dc8f44f
thatnhudev <thatnhudev@gmail.com>
2021-02-28 16:44:09 -0500

    Cleaned up Import Groceries script

--- script/list_foods.rb
+++ script/list_foods.rb
@@ -1,12 +1,9 @@
 def list_10_foods
-  foods = Food.limit(10)
-  puts "total: #{Food.all.count}"
-  foods.each do |food|
-    puts "food: " + food[:name].to_s
+  foods = Food.pluck(:calories)
+  foods[0..9].each do |food|
+    puts "food: #{food.inspect}"
   end
 end
-
-list_10_foods
 
 def check_json_response
   response = RestClient.get 'https://api.edamam.com/api/food-database/v2/parser?ingr=&app_id=592a7717&app_key=c4d981b34b013776df8cbbb041f03279'
@@ -15,4 +12,21 @@ def check_json_response
   puts "response: #{json}"
 end
 
+def all_food_names
+  food_names = Food.pluck(:name)
+  first_10_names = food_names[0..9]
+  # first_10_names.each do |name|
+    response = RestClient.get "https://api.edamam.com/api/food-database/v2/parser?ingr=#{food_names[0]}&category=generic-foods&app_id=592a7717&app_key=c4d981b34b013776df8cbbb041f03279"
+    json = JSON.parse response
+
+    if !json.nil? || !json.empty?
+      puts "API result: #{json["parsed"][0]}\n\n"
+    else
+      puts "error seeding foods"
+    end
+  # end
+end
+
 # check_json_response
+list_10_foods
+# all_food_names
